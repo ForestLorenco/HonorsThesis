78a75410f1c460c6831ccde5b8beeb68b2acbbf8
Forest Elliott <forest@ip-83-246.wireless.oberlin.edu>
2019-12-12 17:42:03 -0500

    Created functional a3c from miyosudo implementation for mario bros, only works on tf 1.13, lstm only works on 1.12

--- Asynch_network/A3C/a3c.py
+++ Asynch_network/A3C/a3c.py
@@ -1,16 +1,31 @@
+# -*- coding: utf-8 -*-
 import tensorflow as tf
-import numpy as np
-import math
-import gym
-
 import threading
+import numpy as np
 
-import network
-import a3c_thread
-
-import const
-
+import signal
+import random
+import math
+import os
 import time
+
+from network import GameACFFNetwork, GameACLSTMNetwork
+from a3c_training_thread import A3CTrainingThread
+from rmsprop_applier import RMSPropApplier
+
+from constants import ACTION_SIZE
+from constants import PARALLEL_SIZE
+from constants import INITIAL_ALPHA_LOW
+from constants import INITIAL_ALPHA_HIGH
+from constants import INITIAL_ALPHA_LOG_RATE
+from constants import MAX_TIME_STEP
+from constants import CHECKPOINT_DIR
+from constants import LOG_FILE
+from constants import RMSP_EPSILON
+from constants import RMSP_ALPHA
+from constants import GRAD_NORM_CLIP
+from constants import USE_GPU
+from constants import USE_LSTM
 
 
 def log_uniform(lo, hi, rate):
@@ -19,133 +34,127 @@ def log_uniform(lo, hi, rate):
   v = log_lo * (1-rate) + log_hi * rate
   return math.exp(v)
 
+device = "/cpu:0"
+if USE_GPU:
+  device = "/gpu:0"
 
-def _anneal_learning_rate(global_time_step):
-    learning_rate = initial_learning_rate * (
-            const.MAX_TIME_STEP - global_time_step) / const.MAX_TIME_STEP
-    if learning_rate < 0.0:
-        learning_rate = 0.0
-    return learning_rate
-
-ENV = gym.make(const.GAME)
-ACTIONS = ENV.action_space.n
-
-initial_learning_rate = log_uniform(const.INITIAL_ALPHA_LOW,
-                                    const.INITIAL_ALPHA_HIGH,
-                                    const.INITIAL_ALPHA_LOG_RATE)
-
-current_lr = initial_learning_rate
-
-global_network = network.Network(ACTIONS, -1)
+initial_learning_rate = log_uniform(INITIAL_ALPHA_LOW,
+                                    INITIAL_ALPHA_HIGH,
+                                    INITIAL_ALPHA_LOG_RATE)
 
 global_t = 0
 
-lr_input = tf.placeholder("float")
+stop_requested = False
 
-#Define stuff for optimization
-#Action input for policy
-pi = global_network.pi
-a = tf.compat.v1.placeholder("float", [None, ACTIONS])
+if USE_LSTM:
+  global_network = GameACLSTMNetwork(ACTION_SIZE, -1, device)
+else:
+  global_network = GameACFFNetwork(ACTION_SIZE, -1, device)
 
-# temporary difference (R-V) (input for policy)
-temp_diff = tf.compat.v1.placeholder("float", [None])
 
-# avoid NaN with clipping when value in pi becomes zero
-log_pi = tf.log(tf.clip_by_value(pi, 1e-20, 1.0))
+training_threads = []
 
-# policy entropy
-entropy = -tf.reduce_sum(pi * log_pi, reduction_indices=1)
+learning_rate_input = tf.placeholder("float")
 
-# policy loss (output)  (Adding minus, because the original paper's objective function is for gradient ascent, but we use gradient descent optimizer.)
-policy_loss = - tf.reduce_sum(
-    tf.reduce_sum(tf.multiply(log_pi, a), reduction_indices=1) * temp_diff + entropy * const.ENTROPY_BETA)
+grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
+                              decay = RMSP_ALPHA,
+                              momentum = 0.0,
+                              epsilon = RMSP_EPSILON,
+                              clip_norm = GRAD_NORM_CLIP,
+                              device = device)
 
-#input for the value function
-r = tf.compat.v1.placeholder("float", [None])
+for i in range(PARALLEL_SIZE):
+  training_thread = A3CTrainingThread(i, global_network, initial_learning_rate,
+                                      learning_rate_input,
+                                      grad_applier, MAX_TIME_STEP,
+                                      device = device)
+  training_threads.append(training_thread)
 
-# value loss (output)
-# (Learning rate for Critic is half of Actor's, so multiply by 0.5)
-value_loss = 0.5 * tf.nn.l2_loss(r - global_network.v)
+# prepare session
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=False,
+                                        allow_soft_placement=True))
 
-# gradienet of policy and value are summed up
-total_loss = policy_loss + value_loss
-opt_vars = [a, temp_diff, r, global_network.s]
-train_global = tf.compat.v1.train.RMSPropOptimizer(learning_rate=lr_input, decay=const.RMSP_ALPHA, momentum=0.0, epsilon=const.RMSP_EPSILON).minimize(total_loss)
+init = tf.global_variables_initializer()
+sess.run(init)
 
-threads = []
-#create a new thread
-for i in range(10):
-    thread = a3c_thread.A3C_Thread(i, const.MAX_TIME_STEP, ACTIONS)
-    threads.append(thread)
+# summary for tensorboard
+score_input = tf.placeholder(tf.int32)
+tf.summary.scalar("score", score_input)
 
-sess = tf.compat.v1.InteractiveSession()
+summary_op = tf.summary.merge_all()
+summary_writer = tf.summary.FileWriter(LOG_FILE, sess.graph)
 
-#initialize a saver to save the model
-saver = tf.compat.v1.train.Saver()
-
-sess.run(tf.compat.v1.global_variables_initializer())
-
-#Restore saved network
-checkpoint = tf.train.get_checkpoint_state("a3c_saves")
+# init or load checkpoint with saver
+saver = tf.train.Saver()
+checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
 if checkpoint and checkpoint.model_checkpoint_path:
-    saver.restore(sess, checkpoint.model_checkpoint_path)
-    print("Loaded Network: {}".format(checkpoint.model_checkpoint_path))
-
-def train(index, lock):
-    global global_t
-    global opt_vars
-    global global_network
-    global a
-    global temp_diff
-    global r
-    global initial_learning_rate
-    global current_lr
-    global lr_input
-    thread = threads[index]
-
-    #make the game
-    lock.acquire()
-    env = gym.make(const.GAME)
-    obs = env.reset()
-    lock.release()
-
-    thread.get_init_obs(obs)
-    while global_t < const.MAX_TIME_STEP:
-        t, vars = thread.step(sess, global_t, global_network, lock, saver, env)
-
-        current_lr = _anneal_learning_rate(global_t)
-
-        global_t += t
-        batch_si, batch_a, batch_td, batch_R = vars[0], vars[1], vars[2], vars[3]
-
-        lock.acquire()
-        sess.run(train_global,
-                 feed_dict={a: batch_a,
-                            r: batch_R,
-                            temp_diff: batch_td,
-                            global_network.s: batch_si,
-                            lr_input: current_lr
-                            })
-        lock.release()
+  saver.restore(sess, checkpoint.model_checkpoint_path)
+  print("checkpoint loaded:", checkpoint.model_checkpoint_path)
+  tokens = checkpoint.model_checkpoint_path.split("-")
+  # set global step
+  global_t = int(tokens[1])
+  print(">>> global step set: ", global_t)
+  # set wall time
+  wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
+  with open(wall_t_fname, 'r') as f:
+    wall_t = float(f.read())
+else:
+  print("Could not find old checkpoint")
+  # set wall time
+  wall_t = 0.0
 
 
+def train_function(parallel_index):
+  global global_t
+  
+  training_thread = training_threads[parallel_index]
+  # set start_time
+  start_time = time.time() - wall_t
+  training_thread.set_start_time(start_time)
 
-if __name__ == "__main__":
-    train_threads = []
-    lock = threading.Lock()
-    for i in range(10):
-        train_threads.append(threading.Thread(target=train, args=(i,lock)))
-    start = time.time()
+  while True:
+    if stop_requested:
+      break
+    if global_t > MAX_TIME_STEP:
+      break
 
-    # start all the threads
-    for x in train_threads:
-        x.start()
+    diff_global_t = training_thread.process(sess, global_t, summary_writer,
+                                            summary_op, score_input)
+    global_t += diff_global_t
+    
+    
+def signal_handler(signal, frame):
+  global stop_requested
+  print('You pressed Ctrl+C!')
+  stop_requested = True
+  
+train_threads = []
+for i in range(PARALLEL_SIZE):
+  train_threads.append(threading.Thread(target=train_function, args=(i,)))
+  
+signal.signal(signal.SIGINT, signal_handler)
 
-    # Join them all when they are done
-    for x in train_threads:
-        x.join()
+# set start time
+start_time = time.time() - wall_t
 
-    end = time.time()
-    print("Model took {} time to train".format(end-start))
+for t in train_threads:
+  t.start()
 
-    saver.save(sess, 'a3c_saves/' + 'checkpoint', global_step=global_t)
+print('Press Ctrl+C to stop')
+signal.pause()
+
+print('Now saving data. Please wait')
+  
+for t in train_threads:
+  t.join()
+
+if not os.path.exists(CHECKPOINT_DIR):
+  os.mkdir(CHECKPOINT_DIR)  
+
+# write wall time
+wall_t = time.time() - start_time
+wall_t_fname = CHECKPOINT_DIR + '/' + 'wall_t.' + str(global_t)
+with open(wall_t_fname, 'w') as f:
+  f.write(str(wall_t))
+
+saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
