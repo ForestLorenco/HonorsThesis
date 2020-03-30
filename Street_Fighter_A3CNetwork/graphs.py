2de4102f44099975c57292d31ebc2eadac72d582
Forest Elliott <forest@Forests-MacBook-Pro-2.local>
2020-03-30 14:14:37 -0400

    fixed graph titles

--- Street_Fighter_A3CNetwork/graphs.py
+++ Street_Fighter_A3CNetwork/graphs.py
@@ -4,23 +4,22 @@ import numpy as np
 
 #Global path variable names
 
-NOSKIP = "NoSkipNetwork"
-SKIP = "SkipNetwork"
+NOSKIP = "NoSkip"
+SKIP = "Skip"
 DEULING = "Dueling"
 
 def SvsNS():
-    paths = ["LSTM/", "NewRew/", "OldRew/"]
+    paths = ["NetworkLSTM/", "NetworkNewRew/", "NetworkOldRew/", "Dueling/"]
+    names = ["LSTM", "R2 Reward", "R1 Reward", "Dueling"]
+    i = 0
     for p in paths:
         #individual
         data_ns = pd.read_csv(NOSKIP+p+"data.csv")
         points_ns = data_ns.values
-        if p == paths[2]:
-            for i in range(len(points_ns)):
-                if points_ns[i] > 0:
-                    points_ns[i] += 176
+        
 
         plt.plot(points_ns, linewidth=.5)
-        plt.title(NOSKIP+p[:-1])
+        plt.title("No Skip "+names[i])
         plt.ylabel('Reward')
         plt.xlabel('Games over Time (25 hrs)')
         
@@ -30,29 +29,31 @@ def SvsNS():
 
         data_s = pd.read_csv(SKIP+p+"data.csv")
         points_s = data_s.values
-        if p == paths[2]:
-            for i in range(len(points_ns)):
-                if points_ns[i] > 0:
-                    points_ns[i] += 176
-                    
+        
+
         plt.plot(points_s, linewidth=.5)
-        plt.title(NOSKIP+p[:-1])
+        plt.title("Skip "+names[i])
         plt.ylabel('Reward')
         plt.xlabel('Games over Time (25 hrs)')
         
-        plt.savefig("Plots/"+SKIP+p[:-1]+"Graph")
+        plt.savefig("Plots/"+SKIP+p[7:-1]+"Graph")
         #plt.show()
         plt.clf()
 
+        if p == paths[3]:
+            points_s = points_s[0:len(points_ns)+150]
+            p = "Network" + p
         #both 
+        print(p[7:-1])
         plt.plot(points_s, linewidth=.5, label="Skip")
         plt.plot(points_ns, linewidth=.5, label="No Skip")
-        plt.title("Skip vs No Skip of"+p[:-1])
+        plt.title("Skip vs No Skip of "+p[7:-1])
         plt.ylabel('Reward')
         plt.xlabel('Games over Time (25 hrs)')
         plt.legend(loc="best")
-        plt.savefig("Plots/S_vs_NS"+p[:-1]+"Graph")
+        plt.savefig("Plots/S_vs_NS"+p[7:-1]+"Graph")
         plt.clf()
+        i += 1
 
 def LSTMvsFF():
     paths = ["LSTM/", "NewRew/"]
@@ -88,14 +89,14 @@ def duelvsa3c():
     plt.clf()
 
     data_d = pd.read_csv(DEULING+"Skip/data.csv")
-    points_d = data_d.values
+    points_d = data_d.values[0:300]
 
     data_a = pd.read_csv(SKIP+"OldRew/data.csv")
     points_a = data_a.values
 
     plt.plot(points_d, linewidth=.5, label="Dueling")
     plt.plot(points_a, linewidth=.5, label="A3C")
-    plt.title("Dueling vs A3C No Skip")
+    plt.title("Dueling vs A3C Skip")
     plt.ylabel('Reward')
     plt.xlabel('Games over Time (25 hrs)')
     plt.legend(loc="best")
@@ -103,8 +104,8 @@ def duelvsa3c():
     plt.clf()
 
 if __name__ == "__main__":
-    #SvsNS()
+    SvsNS()
     #LSTMvsFF()
-    duelvsa3c()
+    #duelvsa3c()
 
 
